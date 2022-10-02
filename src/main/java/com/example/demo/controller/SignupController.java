99f746b14f644a317a66fb6c429af13e8204a9b4
switch <chisui.0421@gmail.com>
2022-10-02 20:43:17 +0900

    chapter6 fin.

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -6,11 +6,16 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.form.GroupOrder;
+import com.example.demo.form.SignupForm;
 
 @Controller
 @RequestMapping("/user")
@@ -20,7 +25,7 @@ public class SignupController {
 	
 	//登録画面を表示
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale) {
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm signupForm) {
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		return "user/signup";
@@ -28,7 +33,13 @@ public class SignupController {
 	
 	//ユーザ登録処理
 	@PostMapping("/signup")
-	public String postSignup() {
+	public String postSignup(Model model, Locale locale, 
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm signupForm, 
+			BindingResult bindingResult) {
+		//入力チェック
+		if(bindingResult.hasErrors()) {
+			return getSignup(model, locale, signupForm);
+		}
 		//ログイン画面にリダイレクト
 		return "redirect:/login";
 	}
